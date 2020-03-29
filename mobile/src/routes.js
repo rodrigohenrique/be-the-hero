4b263f105bd1a91ffd5633c8227d11680b1fee8a
Rodrigo Monteiro (PC FM) <rodrigo99monteiro@gmail.com>
2020-03-28 22:29:01 -0300

    Cria Mobile

---
+++ mobile/src/routes.js
@@ -0,0 +1,25 @@
+import { NavigationContainer } from '@react-navigation/native';
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+
+import Detail from './pages/Detail';
+import Incidents from './pages/Incidents';
+
+const AppStack = createStackNavigator();
+
+export default function Routes() {
+    return (
+        <NavigationContainer>
+            <AppStack.Navigator screenOptions={{
+                headerShown: false
+            }}>
+                <AppStack.Screen name="Incidents" component={Incidents}>
+
+                </AppStack.Screen>
+                <AppStack.Screen name="Detail" component={Detail}>
+
+                </AppStack.Screen>
+            </AppStack.Navigator>
+        </NavigationContainer>
+    );
+}
